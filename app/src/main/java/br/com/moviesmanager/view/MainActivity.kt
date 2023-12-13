782d47a20afe8ee19565d0dd98c4eaf85076b285
WILLIAM ANSELMO <WILLIAMANSELMO@FRANCA.SP.GOV.BR>
2023-12-13 08:34:24 -0300

    Update parts of fragment's xml

--- app/src/main/java/br/com/moviesmanager/view/MainActivity.kt
+++ app/src/main/java/br/com/moviesmanager/view/MainActivity.kt
@@ -3,10 +3,17 @@ package br.com.moviesmanager.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.moviesmanager.R
+import br.com.moviesmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.mainTb)
+        supportActionBar?.title = getString(R.string.app_name)
     }
 }
