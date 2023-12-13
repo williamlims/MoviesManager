cd9896aa6188c262bf7de70100b213d626011889
WILLIAM ANSELMO <WILLIAMANSELMO@FRANCA.SP.GOV.BR>
2023-12-13 10:12:56 -0300

    Set context menu and some changes in MainController, MovieFragment and strings xml

--- app/src/main/java/br/com/moviesmanager/controller/MainController.kt
+++ app/src/main/java/br/com/moviesmanager/controller/MainController.kt
@@ -1,14 +1,41 @@
 package br.com.moviesmanager.controller
 
+import androidx.room.Room
 import br.com.moviesmanager.databinding.FragmentMainBinding
+import br.com.moviesmanager.model.database.MoviesManagerDatabase
+import br.com.moviesmanager.model.database.MoviesManagerDatabase.Companion.MOVIE_DATABASE
 import br.com.moviesmanager.model.entity.Movie
 import br.com.moviesmanager.view.MainFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainController(private val mainFragment: MainFragment) {
-    private lateinit var fmb: FragmentMainBinding
+    private val movieDaoImpl = Room.databaseBuilder(
+        mainFragment.requireContext(),
+        MoviesManagerDatabase::class.java,
+        MOVIE_DATABASE
+    ).build().getMovieDao()
 
-    private val movieList: MutableList<Movie> = mutableListOf()
+    fun insertMovie(movie: Movie) {
+        CoroutineScope(Dispatchers.IO).launch {
+            movieDaoImpl.insertMovie(movie)
+        }
+    }
+
+    fun getMovies() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val movies = movieDaoImpl.selectMovies()
+            //mainFragment.sele(movies)
+        }
+    }
+
+    fun editMovie(movie: Movie) {
+        CoroutineScope(Dispatchers.IO).launch {
+            movieDaoImpl.updateMovie(movie)
+        }
+    }
 
 
 }
--- app/src/main/java/br/com/moviesmanager/view/MovieFragment.kt
+++ app/src/main/java/br/com/moviesmanager/view/MovieFragment.kt
@@ -1,14 +1,23 @@
 package br.com.moviesmanager.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import br.com.moviesmanager.R
 import br.com.moviesmanager.databinding.FragmentMovieBinding
+import br.com.moviesmanager.model.entity.Movie
+import br.com.moviesmanager.model.entity.Movie.Companion.WATCHED_FALSE
+import br.com.moviesmanager.model.entity.Movie.Companion.WATCHED_TRUE
 
 class MovieFragment : Fragment() {
     private lateinit var ftb: FragmentMovieBinding
@@ -23,27 +32,47 @@ class MovieFragment : Fragment() {
 
         ftb = FragmentMovieBinding.inflate(inflater, container, false)
 
-        val receivedTask = navigationArgs.movie
-        receivedTask?.also { task ->
+        val receivedMovie = navigationArgs.movie
+        receivedMovie?.also { movie ->
             with(ftb) {
-                nameEt.setText(task.name)
-                doneCb.isChecked = task.done == TASK_DONE_TRUE
-                navigationArgs.editTask.also { editTask ->
-                    nameEt.isEnabled = editTask
-                    doneCb.isEnabled = editTask
-                    saveBt.visibility = if (editTask) VISIBLE else GONE
+                nameEt.setText(movie.name)
+                releasedEt.setText(movie.released)
+                producerEt.setText(movie.producer)
+                durationEt.setText(movie.duration)
+                watchedCb.isChecked = movie.watched == WATCHED_TRUE
+                noteEt.setText(movie.note)
+
+                ArrayAdapter.createFromResource(
+                    genderSP.context,
+                    R.array.gender_array,
+                    android.R.layout.simple_spinner_item
+                ).also { adapter ->
+                    // Specify the layout to use when the list of choices appears
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+                    // Apply the adapter to the spinner
+                    genderSP.adapter = adapter
+                }
+
+                navigationArgs.editMovie.also { editMovie ->
+                    nameEt.isEnabled = editMovie
+                    releasedEt.isEnabled = editMovie
+                    producerEt.isEnabled = editMovie
+                    durationEt.isEnabled = editMovie
+                    watchedCb.isEnabled = editMovie
+                    noteEt.isEnabled = editMovie
+                    saveBt.visibility = if (editMovie) VISIBLE else GONE
                 }
             }
         }
 
         ftb.run {
             saveBt.setOnClickListener {
-                setFragmentResult(TASK_FRAGMENT_REQUEST_KEY, Bundle().apply {
+                setFragmentResult(MOVIE_FRAGMENT_REQUEST_KEY, Bundle().apply {
                     putParcelable(
-                        EXTRA_TASK, Task(
-                            receivedTask?.time ?: System.currentTimeMillis(),
+                        EXTRA_MOVIE, Movie(
+                            receivedMovie?.time ?: System.currentTimeMillis(),
                             nameEt.text.toString(),
-                            if (doneCb.isChecked) TASK_DONE_TRUE else TASK_DONE_FALSE
+                            if (watchedCb.isChecked) WATCHED_TRUE else WATCHED_FALSE
                         )
                     )
                 })
